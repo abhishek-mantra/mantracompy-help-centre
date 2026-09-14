6c04f7b054471e72eebddf0893825929d3285004
Abhishek <abhishek.madaan@mantra.care>
2026-09-14 17:04:21 +0530

    fix: import useMemo in ArticlePage and enforce tsc in build

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useParams, Link, Navigate } from "react-router";
 import { getArticleBySlug, getArticlesBySection } from "../data/articleRegistry";
 import { getCategoryBySlug } from "../data/categories";
